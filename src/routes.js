af2349a2a3798e2a8ef1f209863b12ceb7c9d044
jeffersonbejarano <jb.18@hotmail.com>
2021-08-20 16:22:35 -0300

    chore: refactoring the folder structure

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const routes = express.Router()
+
+
+routes.get("/", (request, response) => {
+    response.status(200).json({ message: 'Hello gokursos'});
+
+});
+
+export default routes
+
